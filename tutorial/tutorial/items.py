fe4c611a5a1b93a971fbe0c25e46c44d62a0c077
Maogui Lin <linmaog@mail2.sysu.edu.cn>
2016-03-30 21:46:29 +0800

    Update items.py

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -12,3 +12,7 @@ class TutorialsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class YoujiItem(scrapy.Item):
+    title = scrapy.Field()
+    link = scrapy.Field()
